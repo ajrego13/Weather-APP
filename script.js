f9de6ec5fa5d1112df6f54684d477e5e7d8a8299
ajrego13 <83869597+ajrego13@users.noreply.github.com>
2022-11-10 11:55:06 -0500

    updated UI

--- script.js
+++ script.js
@@ -4,73 +4,41 @@ const api = {
     baseURL : 'https://api.openweathermap.org/data/2.5/'
 }
 
-const searchbox = document.querySelector('.search-box')
-searchbox.addEventListener('keypress', setQuery); 
 
-function setQuery (evt) {
-    evt.keyCode == 13 ? getResults(searchbox.value) : ''
-    
-}
+const button = document.getElementById('button').addEventListener('click', () => {
+    return getResults()
+}); 
 
-const getResults = async (query) => {
+
+const getResults = async () => {
+    let query = document.getElementById('input').value
     const response = await fetch(`${api.baseURL}weather?q=${query}&units=imperial&appid=${api.key}`); 
     const weatherData = await response.json();
     console.log(query); 
-    sessionStorage.setItem('location', query); 
+    console.log(weatherData)
+   changeElements(weatherData);
+}
+
+
+const changeElements = (data) => {
+    const cityEl = document.getElementById('city'); 
+    const tempEl = document.getElementById('temp');
+    const image = document.getElementById('image'); 
+    const descriptionEl = document.getElementById('description'); 
+    const humidityEl = document.getElementById('humidity'); 
+    const windEl = document.getElementById('wind'); 
+    let temperature = Math.floor(data.main.temp)
+    let windSpeed = Math.floor(data.wind.speed)
    
-    changeImage(weatherData);
-    changeMain(weatherData)
-    changeTemp(weatherData)
-    changeDesc(weatherData)
-    changeLocation(weatherData)
+
+   cityEl.innerText = 'Weather in' + " " + data.name;
+   tempEl.innerText = temperature + '°F'
+   changeImage(data); 
+   descriptionEl.innerText = data.weather[0].description
+   humidityEl.innerText = data.main.humidity + "%"
+   windEl.innerText = windSpeed + ' ' + 'mph'
+
 }
-
-const savedLocations = () => {
-    const mainDiv = document.getElementById('mainEl'); 
-   let newDiv = document.createElement('div'); 
-   let oldLocations = sessionStorage.getItem('location')
-   console.log(oldLocations)
-   newDiv.innerText = 'Previous Searched Location:' + " " + oldLocations;
-   newDiv.id = 'newDiv'
-   
-   newDiv.style.alignItems = 'center';
-   newDiv.style.padding = '5px'
-   newDiv.style.border = '1px solid'
-   newDiv.style.borderRadius = '25px'
-   newDiv.style.backgroundColor = 'lightblue'
-   newDiv.style.alignItems = 'center'
-
-   mainDiv.append(newDiv); 
-
-   newDiv.addEventListener('click', () => {
-    getResults(oldLocations)
-   })
-}
-
-window.addEventListener('load', (event) => {
-    navigator.geolocation ? navigator.geolocation.getCurrentPosition(setPosition) : ''; 
-    savedLocations();
-})
-
-
-const setPosition = async (position) => {
-    const latitude = await position.coords.latitude;
-    const longitude = await position.coords.longitude; 
-    return getWeatherFunc(latitude, longitude);
-}
-
-
-const getWeatherFunc = async (lat, lon) => {
-  const response = await fetch(`${api.baseURL}weather?lat=${lat}&lon=${lon}&appid=${api.key}&units=imperial`); 
-  const weatherData = await response.json();
-  console.log(weatherData);
-  changeImage(weatherData);
-  changeMain(weatherData)
-  changeTemp(weatherData)
-  changeDesc(weatherData)
-  changeLocation(weatherData)
-}
-
 
 function changeImage (data) {
     const image = document.getElementById('image'); 
@@ -79,27 +47,6 @@ function changeImage (data) {
     image.src = baseImageSrc;
 }
 
-function changeMain (data) {
-    const main = document.getElementById('main'); 
-    let mainData = data.weather[0].main; 
-    main.innerText = mainData;
-}
 
-function changeTemp (data) {
-    const tempEl = document.getElementById('temp');
-    let temperature = data.main.temp; 
-    let roundedTemp = Math.floor(temperature)
-    temp.innerText = roundedTemp;
-}
 
-function changeDesc (data) {
-    const descriptionEl = document.getElementById('description'); 
-    let descript = data.weather[0].description;
-    descriptionEl.innerText = descript;
-}
  
-function changeLocation (data) {
-    const locationEl = document.getElementById('location'); 
-    let locationData = data.name; 
-    locationEl.innerText = locationData;
-}
