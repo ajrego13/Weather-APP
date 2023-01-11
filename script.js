390aed97ed341ca98be1fd811131f910001f6f3c
ajrego13 <83869597+ajrego13@users.noreply.github.com>
2023-01-11 12:22:08 -0500

    made minor edits

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const api = {
 
 
 
-//this section querys the API using the device's geolocation
+//querys the API using the device's geolocation
 window.onload =  () => {
     navigator.geolocation.getCurrentPosition(getResultsWithGeo);
   
 }
 
+// callback function for geolocation function to fetch data from API
 const getResultsWithGeo = async (position) => {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude ;
@@ -20,29 +21,25 @@ const getResultsWithGeo = async (position) => {
     changeElements(weatherData); 
    }
 
-
-
-
 // this section querys the API using the searchbox
  const button = document.getElementById('button').addEventListener('click', () => {
     return getResults()
 }); 
 
+// api request
 const getResults = async () => {
-   
-    try {
+   try {
         let query = document.getElementById('input').value
         const response = await fetch(`${api.baseURL}weather?q=${query}&units=imperial&appid=${api.key}`); 
         const weatherData = await response.json();
-        
-    changeElements(weatherData);
-} catch (error) {
+        changeElements(weatherData);
+    } catch (error) {
     alert('city not found, please make sure you follow this format...city, state');
 }
-    0
+    
 }
 
-// dynamically changing teh UI based on Fetched Data
+// dynamically changing the UI based on Fetched Data
 const changeElements = (data) => {
     const cityEl = document.getElementById('city'); 
     const tempEl = document.getElementById('temp');
@@ -68,10 +65,5 @@ function changeImage (data) {
     let img = data.weather[0].icon;
     const baseImageSrc =  `http://openweathermap.org/img/wn/${img}@2x.png`
     image.src = baseImageSrc;
+   
 }
-
-
-
- 
-
-
